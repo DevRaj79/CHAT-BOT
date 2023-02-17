a248f9c90c6cd9338a768e711ba256899e342630
Dev Raj <114819534+DevRaj79@users.noreply.github.com>
2023-02-17 19:05:31 +0530

    Add files via upload

---
+++ FAUNA.py
@@ -0,0 +1,25 @@
+# module section
+
+
+# Creating a function
+def intake():
+    # taking user input with input command and storing it in data variable change this
+    while True:
+        data = input("USER SAID : ")
+        if "hello" in data:
+            print("hello,sir")
+
+        if "how are you" in data:
+            print("I am fine sir")
+
+        if "hi" in data:
+            print("Hey there")
+
+        if "hey" in data:
+            print("hi bro")
+
+        # you can create your own if condition. be creative!!
+
+
+# calling function(s)
+intake()
